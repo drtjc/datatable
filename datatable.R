d79b76b25e7232b39f31fdfa69c800251b4e13b0
Tom Curnock <tom@greenlightre.ky>
2018-10-03 19:05:09 -0500

    more stuff

--- datatable.R
+++ datatable.R
@@ -416,7 +416,7 @@ attributes(ans)
 
 
 
-################################################################################3
+################################################################################
 # SECONDARY INDEXING AND AUTO INDEXING
 flights <- fread("flights14.csv")
 flights
@@ -431,3 +431,53 @@ setindex(flights, origin, dest)
 indices(flights)
 
 
+###########################################################################################
+# Efficient reshaping using data.tables
+flights <- fread("flights14.csv")
+flights
+
+s1 <- "family_id age_mother dob_child1 dob_child2 dob_child3
+1         30 1998-11-26 2000-01-29         NA
+2         27 1996-06-22         NA         NA
+3         26 2002-07-11 2004-04-05 2007-09-02
+4         32 2004-10-10 2009-08-27 2012-07-21
+5         29 2000-12-05 2005-02-28         NA"
+
+DT <- fread(s1)
+DT
+
+
+DT.m1 = melt(DT, id.vars = c("family_id", "age_mother"),
+             measure.vars = c("dob_child1", "dob_child2", "dob_child3"), variable.name = "child", value.name = "dob")
+DT.m1
+
+dcast(DT.m1, family_id + age_mother ~ child, value.var = "dob")
+
+
+dcast(DT.m1, family_id ~., value.var = "dob")
+
+
+s2 <- "family_id age_mother dob_child1 dob_child2 dob_child3 gender_child1 gender_child2 gender_child3
+1         30 1998-11-26 2000-01-29         NA             1             2            NA
+2         27 1996-06-22         NA         NA             2            NA            NA
+3         26 2002-07-11 2004-04-05 2007-09-02             2             2             1
+4         32 2004-10-10 2009-08-27 2012-07-21             1             1             1
+5         29 2000-12-05 2005-02-28         NA             2             1            NA"
+DT <- fread(s2)
+DT
+
+
+DT.m1 = melt(DT, id = c("family_id", "age_mother"))
+DT.m1
+DT.m1[, c("variable", "child") := tstrsplit(variable, "_", fixed = TRUE)]
+DT.c1 = dcast(DT.m1, family_id + age_mother + child ~ variable, value.var = "value")
+DT.c1
+
+colA = paste("dob_child", 1:3, sep = "")
+colA
+
+
+colA = paste("dob_child", 1:3, sep = "")
+colB = paste("gender_child", 1:3, sep = "")
+DT.m2 = melt(DT, id.vars = c("family_id", "age_mother"), measure.vars = list(colA, colB), value.name = c("dob", "gender"))
+DT.m2
